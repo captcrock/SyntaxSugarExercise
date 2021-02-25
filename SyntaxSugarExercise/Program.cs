8b8cd1238801738d5e65177ce0452f1c83afd131
captcrock <crocker.j.adam@gmail.com>
2021-02-24 19:36:01 -0600

    initial commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "greater than or equal to nine";
+            //}
+            //Console.WriteLine(response);
+
+            var response = answer < 9 ? $"{answer} is less than nice" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+        }
+    }
+}
